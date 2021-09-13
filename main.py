dc6723faf1f0a7162f9be5e08eb8ea54de6470ee
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-13 16:01:52 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+array = np.arange(1, 19)
+print (array)
+print (array.shape)
+
+print (array.reshape(18, 1).shape)
+print (array.reshape(1, 18).shape)
+
+print (array.reshape(3, 2, 3).shape)
+
+
+# inteiros = np.random.randint (1, 6, 5)
+# print (f'A coleção inteiros armazena elementos do tipo  {inteiros.dtype}')
+# reais = np.random.randn(2, 6)
+# print (f'A coleção reais armazena elementos do tipo {reais.dtype}')
+
+# array = np.random.randint (1, 5, 10)
+# print(array)
+# print('***********')
+# matriz = np.reshape (array, (2, 5))
+# print (matriz)
+# print('***********')
+# # max = maior valor argmax = posicao em que se encontra o maior valor
+# # min // e argmin //
+#
+# print(f'matriz[{matriz.argmin()}] = {matriz.min()}')
+# print (f'matriz[{matriz.argmax()}] = {matriz.max()}')
+
+
+# array = np.arange(1, 19)
+# print (array)
+# print('***********')
+# print (array.reshape(3, 3, 2))
+# print('***********')
+# print (array.reshape(3, 1, 6))
+
+# array = np.arange(10)
+# print (array)
+# print('***********')
+# print (np.reshape(array, (2, 5)))
+# print('***********')
+# print (np.reshape(array, (5, 2)))
+# print('***********')
+# print (np.reshape(array, (3, 4)))
+
+#
+# np.random.seed(42)
+#
+# print (np.random.randint(5))
+
+
+#
+# print (np.random.randint(1, 6, (2, 3)))
+
+
+# print (np.random.randint(1, 11, 10))
+
+# # [0, 2)
+# print (np.random.randint(2))
+#
+# # [0, 1)
+# print (np.random.randint(1))
+#
+# print (np.random.randint(0, 11))
+#
+# print (np.random.randint(2, 5))
+
+#
+# print (np.random.randn(2, 5))
+
+# [0, 1)
+# print (np.random.rand())
+# print('***********')
+# print (np.random.rand(1))
+# print('***********')
+# print (np.random.rand(10))
+# print('***********')
+# print (np.random.rand(2, 4))
+# print('***********')
+
+
+
+# print (np.flipud(np.eye(5, 5, 1)))
+
+# print (np.flipud(np.eye(5, 5)))
+
+# print (np.eye(5, 5, -2))
+
+# print (np.eye(3, 5, 1))
+#
+# print('***********')
+#
+# print (np.eye(5, 5, 2))
+#
+# print('***********')
+#
+# print (np.eye(5, 5, 4))
+#
+# print('***********')
+# print(np.eye(5, 5, 5))
+#
+# print('***********')
+# print (np.eye (5, 5, 6))
+
+# print (np.eye(3, 5, 0))
+
+# print (np.eye(4, 5))
+# print('***********')
+# print (np.eye(2, 5))
+# print (np.eye(3))
+
+# print (np.identity(5))
+
+# print (np.identity(4))
+
+
+# print (np.linspace(5,1, 11))
+
+
+# print (np.linspace(5, 5, 10))
+
+
+# print (np.linspace(1, 2))
+
+# print (np.linspace(0, 10, 5))
+
+# [0, 10]
+# print (np.linspace(0, 10, 2))
+
+# print (np.ones((3, 4)))
+
+
+# print (np.ones(10))
+
+
+
+# print (np.zeros((2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2,2, 2, 2, 2, 2)))
+
+
+# print (np.zeros((3, 3, 3)))
+
+# print (np.zeros((2, 4, 3)))
+
+# print (np.zeros ((3, 5)))
+
+# print (np.zeros((6, 6)))
+
+# print (np.zeros(10))
+
+# # [0, 3)
+# de_zero_a_dois = np.arange(3)
+# print (de_zero_a_dois)
+#
+# #[2,6)
+# de_dois_a_seis = np.arange(2, 6)
+# print (de_dois_a_seis)
+#
+# dois_a_onze_de_tres_em_tres = np.arange(2, 12, 3)
+# print (dois_a_onze_de_tres_em_tres)
+
+
+
+# matriz = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+#
+# print (type(matriz))
+# array_duas_dimensoes = np.array(matriz)
+# print (type(array_duas_dimensoes))
+
+
+# lista = [1, 2, 3, 4, 5, 6]
+# print (type(lista))
+# array = np.array(lista)
+# print (type(array))
+# print (type(lista))
+
+# print (type(np.array([1, 2])))
+
